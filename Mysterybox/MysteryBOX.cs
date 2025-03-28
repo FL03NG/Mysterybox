4a66f89c1be2b96c950819f7ab2a6a74d78218cf
FL03NG <sih013@edu.zealand.dk>
2025-03-28 09:36:44 +0100

    Add project files.

---
+++ Mysterybox/MysteryBOX.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Mysterybox
+{
+    internal class MysteryBOX
+    {
+        private string _label;
+        private string _contains;
+        private bool _isexplosive;
+
+        public string Label
+        {
+            get { return _label; }
+            set { _label = value; }
+        }
+        public string Contains
+        {
+            get { return _contains; }
+            set { _contains = value; }
+        }
+        public bool IsEplosive
+        {
+            get { return _isexplosive; }
+            set { _isexplosive = value; }
+        }
+
+        public MysteryBOX(string label, string contains, bool isexplsive)
+        {
+            Label = label;
+            Contains = contains;
+            IsEplosive = isexplsive;
+        
+        }
+        public override string ToString() 
+        {
+            return $" {Label}, {Contains}, {IsEplosive}";
+        }
+    }
+}
---
+++ Mysterybox/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Mysterybox
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MysteryBOX mysterybox = new MysteryBOX("Den seje kasse", "kanin med en bombe", true);
+
+            Console.WriteLine(mysterybox);
+        }
+    }
+}
